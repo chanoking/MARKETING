31448e102a66dab1a53edafe839ea2b8e60f81d7
chano <chanhojin94@gmail.com>
2026-03-17 10:24:21 +0900

    State url in cors

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,12 @@ const __dirname = path.dirname(__filename);
 // =======================
 // Middleware
 // =======================
-app.use(cors());
+app.use(cors({
+    origin: [
+    "https://chanoking.com",
+    "https://www.chanoking.com"
+  ]
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(fileUpload());
