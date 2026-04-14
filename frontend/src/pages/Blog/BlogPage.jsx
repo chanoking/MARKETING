4140242a96d930a49c72f681632f958a96080d0d
chano <chanhojin94@kakao.com>
2026-04-14 16:36:56 +0900

    feat: Apply drag to horizontal scroll menu

--- frontend/src/pages/Blog/BlogPage.jsx
+++ frontend/src/pages/Blog/BlogPage.jsx
@@ -9,15 +9,22 @@ export default function BlogPage() {
     const [keywords, setKeywords] = useState([]);
     const [startDate, setStartDate] = useState(null);
     const [endDate, setEndDate] = useState(null);
+    const [isDown, setIsDown] = useState(false);
+    const [startX, setStartX] = useState(0);
+    const [scrollLeft, setScrollLeft] = useState(0);
+
+    useEffect(() => {
+        document.title = "Blog"
+    }, [])
 
     const scrollRef = useRef();
 
-    const scrollLeft = () => {
+    const scrollLeftClick = () => {
         if (!scrollRef.current) return;
         scrollRef.current.scrollBy({ left: -200, behavior: "smooth"});
     };
 
-    const scrollRight = () => {
+    const scrollRightClick = () => {
         if (!scrollRef.current) return;
         scrollRef.current.scrollBy({ left: 200, behavior: "smooth"});
     }
@@ -28,9 +35,31 @@ export default function BlogPage() {
         setEndDate(end);
     };
 
-    useEffect(() => {
-        document.title = "Blog"
-    }, [])
+    const handleMouseDown = (e) => {
+        setIsDown(true);
+        setStartX(e.pageX - scrollRef.current.offsetLeft);
+        setScrollLeft(scrollRef.current.scrollLeft);
+    }
+
+    const handleMouseUp = () => {
+        setIsDown(false);
+    }
+
+    const handleMouseLeave = () => {
+        setIsDown(false);
+    }
+
+    const handleMouseMove = (e) => {
+        if(!isDown) return;
+
+        e.preventDefault();
+
+        const x = e.pageX - scrollRef.current.offsetLeft;
+        const walk = x - startX;
+
+        scrollRef.current.scrollLeft = scrollLeft - walk;
+    }
+
 
     useEffect(() => {
         const fetchItems = async () => {
@@ -50,8 +79,34 @@ export default function BlogPage() {
     return (
         <div className="blog_body">
             <div className="wrapper">
-                <button className="scroll-btn" onClick={scrollLeft}>‹</button>
-                <div className="scroll-container" ref={scrollRef}>
+                <div className = "blog-datepicker">
+                    <DatePicker
+                        selectsRange
+                        startDate={startDate}
+                        endDate={endDate}
+                        onChange={onChange}
+                        isClearable={true}
+                        dateFormat="yyyy-MM-dd"
+                        placeholderText="날짜 범위를 선택하세요"
+                    />
+                </div>
+                <button className="blog-query-btn">조회</button>
+                <button 
+                    className="scroll-btn" 
+                    onClick={scrollLeftClick} 
+                    >‹</button>
+
+                <div 
+                    className="scroll-container"
+                    ref={scrollRef}
+                    style={{
+                        cursor: isDown ? "grabbing" : "grab"
+                     }}
+                     onMouseDown={handleMouseDown}
+                     onMouseUp={handleMouseUp}
+                     onMouseLeave={handleMouseLeave}
+                     onMouseMove={handleMouseMove}
+                >
                 {items.map((item) => (
                     <button
                         key={item._id}
@@ -62,18 +117,7 @@ export default function BlogPage() {
                     </button>
                 ))}
                 </div>
-                <button className="scroll-btn" onClick={scrollRight}>›</button>
-            </div>
-            <div className = "blog-datepicker">
-                <DatePicker
-                    selectsRange
-                    startDate={startDate}
-                    endDate={endDate}
-                    onChange={onChange}
-                    isClearable={true}
-                    dateFormat="yyyy-MM-dd"
-                    placeholderText="날짜 범위를 선택하세요"
-                />
+                <button className="scroll-btn" onClick={scrollRightClick}>›</button>
             </div>
         </div> 
         )
